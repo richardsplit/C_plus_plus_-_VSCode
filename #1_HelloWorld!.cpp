738daf31863f5805ed4c7ae87b1f9cda44347b4b
richardsplit <richard_bg@abv.bg>
2020-12-23 05:06:15 +0200

    #to24_Strings_Calleb_Curry

--- #1_HelloWorld!.cpp
+++ #1_HelloWorld!.cpp
@@ -1,50 +1,14 @@
 #include <iostream>
+#include<climits>
 #include<cmath>
 using std::cout;
 using std::endl;
 using std::cin;//console in
 
 
-//double power (double , int) -- declation [must be at the start of our program ]
-
-/*double power (double base, int exponent ) // definition 
-    {
-    return 0.0;
-    }
-*/
-
-double power (double base, int exponent ) //declaring and defining  ## 'base' & 'exponent' -parameters ##  identifier 'power'
-    {
-     double result =1 ;
-
-        for(int i =0;i<exponent;i++)
-        {
-            result=result * base;
-        }
-        return result;
-    }
-
-
-    void print_pow(double base, int exponent)
-    {
-   
-    double mypower = power (base, exponent); // -'arguments '
-    cout <<base <<"  Raised to the " << exponent <<" power is  "<< mypower<<"\n";
-
-    }
-
-int main () //main function 
-
-
+int main ()
 {
+   bool richard = false  ;
+   cout<<richard<<endl;
 
-double base ;
-int exponent ;
-
-  cout <<"What is the base ?: ";
-    cin >> base; 
-    cout <<"What is the exponent?: ";
-    cin >> exponent;
-
-    print_pow(base,exponent);
 }
