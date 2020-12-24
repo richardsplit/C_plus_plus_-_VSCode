258dd6b0da75f251a2df3c202a53fa2cb91a49f2
richardsplit <richard_bg@abv.bg>
2020-12-24 21:23:32 +0200

    #Until_27_Calleb_Curry

--- #1_HelloWorld!.cpp
+++ #1_HelloWorld!.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<climits>
 #include<cmath>
+#include<string>
 using std::cout;
 using std::endl;
 using std::cin;//console in
@@ -8,7 +9,8 @@ using std::cin;//console in
 
 int main ()
 {
-   bool richard = false  ;
-   cout<<richard<<endl;
+   std::string greeting = "What the hell";
+   greeting.replace (5,3,"Heaven and ") ;
+   cout <<greeting <<endl;
 
 }
